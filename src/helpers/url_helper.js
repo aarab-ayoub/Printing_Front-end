ecc439ce2d9b5c4dfbe3deb67da12ce2f7b3d8b9
ayoub <ayoubaarab571@gmail.com>
2024-05-02 17:18:31 +0100

    common

--- src/helpers/url_helper.js
+++ src/helpers/url_helper.js
@@ -1,6 +1,7 @@
 //REGISTER
 export const POST_FAKE_REGISTER = "/post-fake-register"
 
+
 //LOGIN
 export const POST_FAKE_LOGIN = "/post-fake-login"
 export const POST_FAKE_JWT_LOGIN = "/post-jwt-login"
